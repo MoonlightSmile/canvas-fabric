d01f51298f84394fc727670f0b3a8ea8ec896b58
Moonlight <362652565@qq.com>
2019-02-11 11:42:10 +0800

    Updates

---
+++ precache-manifest.a00109a89a2e3edf42f1b4f12ffe3f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aca025049955b4889f0",
+    "url": "/canvas-fabric/static/js/runtime~main.5aca0250.js"
+  },
+  {
+    "revision": "599d66647056ced75aeb",
+    "url": "/canvas-fabric/static/js/main.599d6664.chunk.js"
+  },
+  {
+    "revision": "94937af028c85fa4d420",
+    "url": "/canvas-fabric/static/js/2.94937af0.chunk.js"
+  },
+  {
+    "revision": "599d66647056ced75aeb",
+    "url": "/canvas-fabric/static/css/main.5f4c0009.chunk.css"
+  },
+  {
+    "revision": "94937af028c85fa4d420",
+    "url": "/canvas-fabric/static/css/2.6e8f965f.chunk.css"
+  },
+  {
+    "revision": "01b6a28984061d59519be071bc96fe39",
+    "url": "/canvas-fabric/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/canvas-fabric/precache-manifest.a00109a89a2e3edf42f1b4f12ffe3f75.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/canvas-fabric/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
